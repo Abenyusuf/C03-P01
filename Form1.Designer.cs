1d32a9a3a34ae2cbf7d10d73209b7a6767b64d57
abeny <abeny@AHMEDBENYUSUF>
2024-11-04 11:46:44 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class BIgCats
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lionlabel = new System.Windows.Forms.Label();
+            this.Tigerlabel = new System.Windows.Forms.Label();
+            this.LionInput = new System.Windows.Forms.TextBox();
+            this.TigerInput = new System.Windows.Forms.TextBox();
+            this.Calculatebutton = new System.Windows.Forms.Button();
+            this.OutputLions = new System.Windows.Forms.Label();
+            this.TotalCatsDisplay = new System.Windows.Forms.Label();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.PercentLions = new System.Windows.Forms.Label();
+            this.PercentTigers = new System.Windows.Forms.Label();
+            this.OutputTigers = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lionlabel
+            // 
+            this.Lionlabel.AutoSize = true;
+            this.Lionlabel.Location = new System.Drawing.Point(135, 48);
+            this.Lionlabel.Name = "Lionlabel";
+            this.Lionlabel.Size = new System.Drawing.Size(191, 20);
+            this.Lionlabel.TabIndex = 0;
+            this.Lionlabel.Text = "Enter the number of lions ";
+            this.Lionlabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Tigerlabel
+            // 
+            this.Tigerlabel.AutoSize = true;
+            this.Tigerlabel.Location = new System.Drawing.Point(135, 104);
+            this.Tigerlabel.Name = "Tigerlabel";
+            this.Tigerlabel.Size = new System.Drawing.Size(198, 20);
+            this.Tigerlabel.TabIndex = 1;
+            this.Tigerlabel.Text = "Enter the number of tigers ";
+            // 
+            // LionInput
+            // 
+            this.LionInput.Location = new System.Drawing.Point(350, 48);
+            this.LionInput.Name = "LionInput";
+            this.LionInput.Size = new System.Drawing.Size(100, 26);
+            this.LionInput.TabIndex = 2;
+            this.LionInput.TextChanged += new System.EventHandler(this.LionInput_TextChanged_1);
+            // 
+            // TigerInput
+            // 
+            this.TigerInput.Location = new System.Drawing.Point(350, 104);
+            this.TigerInput.Name = "TigerInput";
+            this.TigerInput.Size = new System.Drawing.Size(100, 26);
+            this.TigerInput.TabIndex = 3;
+            this.TigerInput.TextChanged += new System.EventHandler(this.Tigerinput_TextChanged);
+            // 
+            // Calculatebutton
+            // 
+            this.Calculatebutton.Location = new System.Drawing.Point(358, 274);
+            this.Calculatebutton.Name = "Calculatebutton";
+            this.Calculatebutton.Size = new System.Drawing.Size(92, 46);
+            this.Calculatebutton.TabIndex = 4;
+            this.Calculatebutton.Text = "Calculate";
+            this.Calculatebutton.UseVisualStyleBackColor = true;
+            this.Calculatebutton.Click += new System.EventHandler(this.Calculatebutton_Click);
+            // 
+            // OutputLions
+            // 
+            this.OutputLions.AutoSize = true;
+            this.OutputLions.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OutputLions.Location = new System.Drawing.Point(345, 148);
+            this.OutputLions.Name = "OutputLions";
+            this.OutputLions.Size = new System.Drawing.Size(111, 22);
+            this.OutputLions.TabIndex = 5;
+            this.OutputLions.Text = "                         ";
+            this.OutputLions.Click += new System.EventHandler(this.OutputDisplay_Click_1);
+            // 
+            // TotalCatsDisplay
+            // 
+            this.TotalCatsDisplay.AutoSize = true;
+            this.TotalCatsDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TotalCatsDisplay.Location = new System.Drawing.Point(345, 186);
+            this.TotalCatsDisplay.Name = "TotalCatsDisplay";
+            this.TotalCatsDisplay.Size = new System.Drawing.Size(111, 22);
+            this.TotalCatsDisplay.TabIndex = 6;
+            this.TotalCatsDisplay.Text = "                         ";
+            this.TotalCatsDisplay.Click += new System.EventHandler(this.TotalCatsDisplay_Click);
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.Location = new System.Drawing.Point(245, 188);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(81, 20);
+            this.TotalLabel.TabIndex = 7;
+            this.TotalLabel.Text = "Total Cats";
+            this.TotalLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // PercentLions
+            // 
+            this.PercentLions.AutoSize = true;
+            this.PercentLions.Location = new System.Drawing.Point(242, 150);
+            this.PercentLions.Name = "PercentLions";
+            this.PercentLions.Size = new System.Drawing.Size(83, 20);
+            this.PercentLions.TabIndex = 8;
+            this.PercentLions.Text = "% of Lions";
+            this.PercentLions.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // PercentTigers
+            // 
+            this.PercentTigers.AutoSize = true;
+            this.PercentTigers.Location = new System.Drawing.Point(482, 150);
+            this.PercentTigers.Name = "PercentTigers";
+            this.PercentTigers.Size = new System.Drawing.Size(88, 20);
+            this.PercentTigers.TabIndex = 10;
+            this.PercentTigers.Text = "% of Tigers";
+            this.PercentTigers.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // OutputTigers
+            // 
+            this.OutputTigers.AutoSize = true;
+            this.OutputTigers.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OutputTigers.Location = new System.Drawing.Point(585, 148);
+            this.OutputTigers.Name = "OutputTigers";
+            this.OutputTigers.Size = new System.Drawing.Size(111, 22);
+            this.OutputTigers.TabIndex = 9;
+            this.OutputTigers.Text = "                         ";
+            this.OutputTigers.Click += new System.EventHandler(this.OutputTigers_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(358, 369);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(92, 46);
+            this.ExitButton.TabIndex = 11;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // BIgCats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.PercentTigers);
+            this.Controls.Add(this.OutputTigers);
+            this.Controls.Add(this.PercentLions);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.TotalCatsDisplay);
+            this.Controls.Add(this.OutputLions);
+            this.Controls.Add(this.Calculatebutton);
+            this.Controls.Add(this.TigerInput);
+            this.Controls.Add(this.LionInput);
+            this.Controls.Add(this.Tigerlabel);
+            this.Controls.Add(this.Lionlabel);
+            this.Name = "BIgCats";
+            this.Text = "Big Cats Exhibit";
+            this.Load += new System.EventHandler(this.BIgCats_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lionlabel;
+        private System.Windows.Forms.Label Tigerlabel;
+        private System.Windows.Forms.TextBox LionInput;
+        private System.Windows.Forms.TextBox TigerInput;
+        private System.Windows.Forms.Button Calculatebutton;
+        private System.Windows.Forms.Label OutputLions;
+        private System.Windows.Forms.Label TotalCatsDisplay;
+        private System.Windows.Forms.Label TotalLabel;
+        private System.Windows.Forms.Label PercentLions;
+        private System.Windows.Forms.Label PercentTigers;
+        private System.Windows.Forms.Label OutputTigers;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿// Ahmed Benyusuf
+// Class and Section (CS 313 01)
+// Assignment (Program 01 Chapter 03)
+// Description of the Program: This program will calculate and display the percentage of lions and tigers
+// I have also added the optional extra credit
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class BIgCats : Form
+    {
+        public BIgCats()
+         {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculatebutton_Click(object sender, EventArgs e)
+        {
+            // This button will complete the required calculations 
+            // These doubles are representing point of storage for floats
+            double Tigers;
+            double Lions;
+            double Total;
+            double PercentTigers;
+            double PercentLions;
+
+            // Converts the inputs from strings to floats / integers
+            Tigers = double.Parse(TigerInput.Text);
+            Lions = double.Parse(LionInput.Text);
+
+            // Calculate the total amount of cats and display them 
+            Total = (Tigers + Lions);
+            TotalCatsDisplay.Text = Total.ToString();
+
+            // calculate the percentage of tigers and display
+            PercentTigers = (Tigers / Total);
+            OutputTigers.Text = PercentTigers.ToString("P1");
+
+            // calculate the percentage of lions and display
+            PercentLions = (Lions / Total);
+            OutputLions.Text = PercentLions.ToString("P1");
+
+        }
+        private void Tigerinput_TextChanged(object sender, EventArgs e)
+        {
+            //This is an input for the amount of Tigers
+        }
+
+        private void LionInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OutputDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LionInput_TextChanged_1(object sender, EventArgs e)
+        {
+            //This is an input for the amount of lions
+        }
+
+        private void OutputDisplay_Click_1(object sender, EventArgs e)
+        {
+            //This is the output for the % of lions
+        }
+
+        private void TotalCatsDisplay_Click(object sender, EventArgs e)
+        {
+            //This is the output for the total amount of cats 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            //This is a button to exit the program
+        }
+
+        private void OutputTigers_Click(object sender, EventArgs e)
+        {
+            //This is the output for the percent of tigers
+        }
+
+        private void BIgCats_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
